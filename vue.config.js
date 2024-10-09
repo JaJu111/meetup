a29363bb48050341c7c07c7f57daa5a80e7b12bf
Jaloliddin Musabayev <musaboyev6@gmail.com>
2024-10-09 14:07:38 +0500

    init

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
     transpileDependencies: [
         'vuetify'
-    ]
+    ],
+    publicPath: process.env.NODE_ENV === 'production' ? '/meetup/' : '/'
 })
